ca4e90e93982a8add5b4c7c4c831260da3d0ee65
SegnonDimas <segnondimas@gmail.com>
2026-04-23 12:24:20 +0100

    Operateur ternaire

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
 void main() {
   //Opérateurs
-  var nombreVie = 10;
 
-  var balle = nombreVie - 1;
-  var bombe = nombreVie - 2;
+  var a = 10;
+  var b = 50;
 
-  nombreVie -= 1; //Si c'est une balle (nombreVie = nombreVie - 1)
-  nombreVie -= 2; //Si c'est une bombe (nombreVie = nombreVie - 2)
-  nombreVie +=
-      1; //Si c'est une potion de vie gagnée (nombreVie = nombreVie + 1)
+  var comparaison = (a > b)
+      ? "A est supérieur à B"
+      : "A est inférieur ou égal à B";
+
+  print(comparaison);
 }
