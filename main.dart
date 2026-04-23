4177cd40818689a1ecca4082d436a2a53b0db60f
SegnonDimas <segnondimas@gmail.com>
2026-04-23 10:27:51 +0100

    Apprentisage du langage Dart : type de donnees

---
+++ main.dart
@@ -0,0 +1,15 @@
+// La fonction principale d'excécution de l'application
+void main() {
+  //Corps de la fonction
+
+  /*
+  DÉCLARATION DES VARIABLES
+
+  type_varible nom_variable = valeur_initiale_variable;
+
+  Ex : int age = 20;
+
+  type_variable : int (entier), double (décimal), String (texte), bool (booléen : true or false), List (liste), Map(dictionnaire), Set(liste ordonnée), etc.
+  
+  */
+}
