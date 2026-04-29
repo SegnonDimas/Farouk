cece16558953c1955438924a9e49d7126e1f8f0d
SegnonDimas <segnondimas@gmail.com>
2026-04-29 12:39:59 +0100

    While : table de multiplication de 9

--- main.dart
+++ main.dart
@@ -3,28 +3,12 @@ import 'dart:io';
 void main() {
   // Structures de contrôle : Boucles => boucle while
 
-  String? pseudo = "user_fg ";
-  String? password = "fg1234";
-
-  // Exécution en une seule fois
-  if (pseudo == "user_fg" && password == "fg1234") {
-    print("Bienvenue $pseudo");
-  } else {
-    print("Identifiants incorrects. Veuillez réessayer.");
+  int nombre = 0;
+  print("TABLE DE MULTIPLICATION DE 9");
+  while (nombre <= 12) {
+    print("9 x ${nombre} = ${9 * nombre}");
+    nombre = nombre + 1;
   }
 
-  // Exécution répétée tant que les identifiants sont incorrects
-  while (pseudo != "user_fg" || password != "fg1234") {
-    print("Identifiants incorrects. Veuillez réessayer.");
-    stdout.write('Entrez votre nom : ');
-    String? _pseudo = stdin.readLineSync();
-
-    stdout.write('Entrez votre mot de passe : ');
-    String? _password = stdin.readLineSync();
-
-    pseudo = _pseudo!;
-    password = _password!;
-  }
-
-  print("Utilisateur connecté avec succès. Bienvenue $pseudo");
+  print("Fin de la table de multiplication de 9");
 }
