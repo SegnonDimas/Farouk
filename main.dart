ebf17dbc878cd897bacd9794fc77dc13f9f4655a
SegnonDimas <segnondimas@gmail.com>
2026-04-29 11:28:44 +0100

    Enum : creation - declaration - utilisation

--- main.dart
+++ main.dart
@@ -1,50 +1,40 @@
+//Type : Enum
+
+// type_variable nom_variable = valeur_initiale;
+/*
+    Pour les enum, la syntaxe est la suivante :
+    enum Nom_variable {valeur1, valeur2, ...}
+*/
+
+enum Jours {
+  Lundi,
+  Mardi,
+  Mercredi,
+  Jeudi,
+  Vendredi,
+  Samedi,
+  Dimanche;
+
+  bool get isWeekend => this == Jours.Samedi || this == Jours.Dimanche;
+}
+
+enum video_status { is_playing, is_paused, is_finished }
+
+enum user_status { is_online, is_offline }
+
+enum ligth_status { is_on, is_off }
+
+// Fonction main : fonction principale
 void main() {
-  // Map : Key-Value Pair
+  // Déclaration d'une variable de type Jours
 
-  //type_variable nom_variable = valeur_initiale;
-  List capitales_pays = ["Porto-Novo", "Lome"];
-  capitales_pays.add("Paris");
+  Jours premier_jour_semaine = Jours.Lundi;
 
-  Map capitales = {'Benin': "Porto-Novo", 'Togo': "Lome"};
+  Jours autre_jour = Jours.Samedi;
 
-  // Map<String, int> : clé de type String et valeur de type int
-  Map<String, int> ages = {"Alice": 30, 'Bob': 25, 'Charlie': 35};
+  Jours jour_anniversaire_jean = Jours.Mercredi;
+  Jours jour_anniversaire_marie = Jours.Dimanche;
 
-  print(capitales_pays[0]);
-  print(capitales["Nigeria"]);
-  print(ages["Alice"]);
-
-  // Map<String, String> : clé de type String et valeur de type String
-  Map<String, String> traductions = {
-    'Hello': 'Salut',
-    'Goodbye': 'Au revoir',
-    'Thank you': 'Merci',
-    'Good morning': 'Bonjour',
-  };
-
-  //Ajout de nouvelles valeurs
-  traductions['Welcome'] = 'Bienvenue';
-
-  print(traductions['Hello']);
-  print(traductions['Goodbye']);
-  print(traductions['Welcome']);
-  print(traductions);
-
-  print(traductions.length); // Affiche les clés du Map
-
-  // Supprime la clé 'Goodbye' et sa valeur associée
-  traductions.remove('Goodbye');
-  print(traductions);
-
-  Map<String, String> mini_tradctions = {
-    'Yes': 'Oui',
-    'No': 'Non',
-    'A': "Un",
-    'For': "Pour",
-  };
-
-  traductions.addAll(mini_tradctions);
-  print(
-    traductions.containsKey("Good morning"),
-  ); // Vérifie si la clé "Good morning" existe dans le Map
+  print(jour_anniversaire_jean.isWeekend); // Affiche false
+  print(jour_anniversaire_marie.isWeekend); // Affiche true
 }
