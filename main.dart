818c5616fa15b44c053ab90abd0ee32f309f9c17
SegnonDimas <segnondimas@gmail.com>
2026-05-04 13:07:32 +0100

    Les fonction et leur utilisation

--- main.dart
+++ main.dart
@@ -1,23 +1,68 @@
 void main() {
-  //Programme calculant la somme des nombre de 1 à 100
-  /* 
-  somme = 1+2+3+....+99+100 : 
-  0+1 = 1
-  1+2 = 3
-  3+3 = 6
-  6+4 = 10
-  10+5= 15
-  15+6= 21
-  21+7= 28
-  ...
-  ..
-  .
-  */
-  int somme = 0;
-
-  for (int nombre = 1; nombre <= 1000; nombre = nombre + 1) {
-    somme = somme + nombre;
+  // fonction avec parametre (String userName), sans type de retour (void)
+  // fonction salutation
+  void salutation(String userName) {
+    print("Bonjour $userName");
   }
 
-  print(somme);
+  // fonction avec paramètre optioniel positionnel [int? age]
+  // fonction presentation
+  void presentation(String nom, String profession, [int? age]) {
+    age != null
+        ? print(
+            "Bonjour, je m'appelle $nom, j'ai $age ans et je suis $profession",
+          )
+        : print("Bonjour, je m'appelle $nom et je suis $profession");
+  }
+
+  // appel à la fonction présentation
+  presentation("Dimas", "Informaticien", 25); // avec l'age
+  presentation("Farouk", "Développeur Mobile"); // sans l'age
+
+  // fonction avec type de retour (int)
+  // fonction calculSomme
+  int calculSomme(int debut, int fin) {
+    int somme = 0;
+    for (int nombre = debut; nombre <= fin; nombre++) {
+      somme = somme + nombre;
+      print(somme);
+    }
+    return somme; // valeur retournée : somme
+  }
+
+  // fonction addition
+  int addition(int a, int b) {
+    print(a + b);
+    return a + b; //doit être obligatoirement la dernière ligne
+    print(a + b); // cette ligne ne sera JAMAIS exécutée
+  }
+
+  // fonction fléchée
+  // fonction multiplication
+  int multiplication(int a, int b) => a * b;
+
+  // appel et utilisation de la fonction multiplication
+  15; // print(15);
+  print(multiplication(7, 5) * 10);
+
+  // autre exemple
+  // fonction pwdGenerator pour générer un mot de pass selon le nom de l'utilisateur
+  void pwdGenerator(String userName) {
+    String pwd = "";
+    pwd =
+        userName[userName.length - 1].toUpperCase() +
+        userName[0].toLowerCase() +
+        userName.length.toString() +
+        userName[1].toUpperCase() +
+        (userName.length - 1).toString() +
+        "@./_"
+            "usd";
+    print(pwd);
+  }
+
+  // appel à la fonction pwdGenerator
+  pwdGenerator("Farouk");
+
+  // appel à la fonction addition
+  addition(4, 50);
 }
