92640e2b63d528334a30da58b3d3472f30106835
SegnonDimas <segnondimas@gmail.com>
2026-05-06 13:16:37 +0100

    Creation des classes Store et Product et leur instantiation

--- main.dart
+++ main.dart
@@ -1,25 +1,43 @@
+import 'dart:io';
+
 void main() {
   // instancier un objet de type User
   // Rappel : déclaration de variable
   // type_variable nom_variable = valeur_initiale;
-  User firstUser = User(
+
+  // création d'une boutique
+  Store store1 = Store(
     id: "1",
-    fullName: "Dimas SEGNON",
-    pseudo: "segmas_dev",
-    account_balance: 1000000000,
-  ); // Ceci est appelé un objet ou une instance de la classe User
+    storeName: "FG Store",
+    description: "I'm a dev store to Benin country",
+    storeLocation: "Benin-Cotonou",
+    products: [],
+  );
 
-  firstUser.consulterSolde();
-  firstUser.consulterCatalogue();
-  firstUser.deposerArgent(3000000000);
+  Product p2 = Product(
+    id: "1",
+    productName: "Yango App",
+    productDescription: "An app for yango agents",
+    productPrice: 1000000,
+  );
 
-  User user2 = User(id: "2", fullName: "Farouk", pseudo: "fg-dev");
-  user2.consulterSolde();
-  user2.deposerArgent(10000000000);
-  user2.retirerArgent(15000000000);
+  Product p1 = Product(
+    id: "1",
+    productName: "Gozem App",
+    productDescription: "An app for gozem agents",
+    productPrice: 1000000,
+  );
+
+  store1.addProduct(p1);
+
+  store1.addProduct(p2);
+
+  store1.getAllProductsNames();
+
+  store1.showStoreInfos();
 }
 
-// class utilisateur
+// classe utilisateur
 class User {
   // attributs
   String id;
@@ -57,4 +75,70 @@ class User {
       print("Retrait effectué avec succès\nNouveau solde : $account_balance");
     }
   }
+}
+
+// classe boutique
+class Store {
+  // attriuts
+  String id;
+  String storeName;
+  String storeLocation;
+  String description;
+  List<Product>? products;
+
+  // constructeur
+  Store({
+    required this.id,
+    required this.storeName,
+    required this.description,
+    required this.storeLocation,
+    this.products,
+  });
+
+  // methodes
+  // ajouter produit
+  void addProduct(Product product) {
+    products!.add(product);
+  }
+
+  // supprimer produit
+  void removeProduct(Product product) {
+    products!.remove(product);
+  }
+
+  // pour récupérer le nom de tous les prduits
+  List<String> productsNames = [];
+  List<String> getAllProductsNames() {
+    for (Product product in this.products!) {
+      productsNames.add(product.productName);
+    }
+    return productsNames;
+  }
+
+  // afficher les informations de la boutique
+  void showStoreInfos() {
+    print("""
+Nom boutique : $storeName
+Description boutique : $description
+Localisation : $storeLocation
+Les produits de la boutique : $productsNames
+""");
+  }
+}
+
+// classe produit
+class Product {
+  // attributs
+  String id;
+  String productName;
+  String productDescription;
+  double productPrice;
+
+  // constructeur
+  Product({
+    required this.id,
+    required this.productName,
+    required this.productDescription,
+    required this.productPrice,
+  });
 }
