49b0b48750ab32bae4e7206b6267ff2d6806ad78
SegnonDimas <segnondimas@gmail.com>
2026-05-06 11:03:25 +0100

    Exercice : calcul de moyenne

--- main.dart
+++ main.dart
@@ -1,24 +1,25 @@
 void main() {
-  //fonction à paramètres nommés
-  void sePresenter({required String nom, String profession = "", int? age}) {
-    age != null
-        ? print(
-            "Bonjour, je m'appelle $nom, j'ai $age ans et je suis $profession",
-          )
-        : print("Bonjour, je m'appelle $nom et je suis $profession");
+  List<int> notes = [18, 12, 13, 19, 17, 8, 10, 14];
+
+  /*
+  moyenne = (somme notes)/(nombre notes)
+  moyenne = (18+12+13+19+17+8)/6
+   */
+  int sommeNotes = 0;
+  int nombreNotes = notes.length;
+  double moyenne = 0.0;
+
+  for (int note in notes) {
+    sommeNotes = sommeNotes + note;
+    // nombreNotes = nombreNotes + 1;
   }
-
-  // appel de la fonction sePresenter()
-  sePresenter(nom: "Kodjo", age: 24, profession: "Dev Mobile");
-
-  // fonction addition
-  int addition([int a = 0, int b = 0]) {
-    print(a + b);
-    return a + b; //doit être obligatoirement la dernière ligne
-    print(a + b); // cette ligne ne sera JAMAIS exécutée
+  /*
+  EQUIVALENCE AVEC LA BOUCLE CLASSIQUE FOR
+  
+  for (int index = 0; index < notes.length; index++) {
+    sommeNotes = sommeNotes + notes[index];
   }
-
-  addition(23, 4); // avec les deux paramètres renseignés
-  addition(3); // avec un seul paramètre
-  addition(); // sans paramètre (aucun paramètre renseigné)
+  */
+  moyenne = sommeNotes / nombreNotes;
+  print(moyenne);
 }
