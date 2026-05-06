5c49d70b1bf7c2634363ac478b3032ad72a782a4
SegnonDimas <segnondimas@gmail.com>
2026-05-06 12:05:43 +0100

    Programmation Orientee Objet avec Dart : creation de class User

--- main.dart
+++ main.dart
@@ -1,25 +1,60 @@
 void main() {
-  List<int> notes = [18, 12, 13, 19, 17, 8, 10, 14];
+  // instancier un objet de type User
+  // Rappel : déclaration de variable
+  // type_variable nom_variable = valeur_initiale;
+  User firstUser = User(
+    id: "1",
+    fullName: "Dimas SEGNON",
+    pseudo: "segmas_dev",
+    account_balance: 1000000000,
+  ); // Ceci est appelé un objet ou une instance de la classe User
 
-  /*
-  moyenne = (somme notes)/(nombre notes)
-  moyenne = (18+12+13+19+17+8)/6
-   */
-  int sommeNotes = 0;
-  int nombreNotes = notes.length;
-  double moyenne = 0.0;
+  firstUser.consulterSolde();
+  firstUser.consulterCatalogue();
+  firstUser.deposerArgent(3000000000);
 
-  for (int note in notes) {
-    sommeNotes = sommeNotes + note;
-    // nombreNotes = nombreNotes + 1;
+  User user2 = User(id: "2", fullName: "Farouk", pseudo: "fg-dev");
+  user2.consulterSolde();
+  user2.deposerArgent(10000000000);
+  user2.retirerArgent(15000000000);
+}
+
+// class utilisateur
+class User {
+  // attributs
+  String id;
+  String? pseudo;
+  String fullName;
+  double account_balance;
+
+  // constructeur
+  User({
+    required this.id,
+    this.pseudo,
+    required this.fullName,
+    this.account_balance = 0.0,
+  });
+
+  // methodes
+  void consulterCatalogue() {
+    print("$fullName est en train de consulter notre catalogue");
   }
-  /*
-  EQUIVALENCE AVEC LA BOUCLE CLASSIQUE FOR
-  
-  for (int index = 0; index < notes.length; index++) {
-    sommeNotes = sommeNotes + notes[index];
+
+  void consulterSolde() {
+    print("Le solde de $fullName est : $account_balance");
   }
-  */
-  moyenne = sommeNotes / nombreNotes;
-  print(moyenne);
+
+  void deposerArgent(double montant) {
+    account_balance = account_balance + montant;
+    print("Dépot effectué avec succès\nNouveau solde : $account_balance");
+  }
+
+  void retirerArgent(double montant) {
+    if (montant > account_balance) {
+      print("Solde inffisant");
+    } else {
+      account_balance = account_balance - montant;
+      print("Retrait effectué avec succès\nNouveau solde : $account_balance");
+    }
+  }
 }
